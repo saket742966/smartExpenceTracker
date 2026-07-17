61c453fb22c71ab79d8422139526fdbaadcbe365
SAKET SINGH <saket742930@gmail.com>
2026-07-17 13:51:16 +0530

    Added Expense ID to each Expense

--- expense_manager.py
+++ expense_manager.py
@@ -1,21 +1,39 @@
 expenses = []
+next_id = 1
 
 def add_expense():
+    global next_id
+    
     category = input("Enter Category of Expense: ")
+    
     description = input("Enter a description: ")
-    amount = int(input("Enter Amount: "))
+    
+    while True:
+        try:
+            amount = int(input("Enter Amount: "))
+            if amount > 0 :
+                break
+            
+        except ValueError:
+            print("Invalid input.")
+
     payment_method = input("Payment Method used: ")
     
     
     expense_data = {
+        "id": next_id,
         "category": category,
         "description": description,
         "amount": amount,
         "payment_method": payment_method
     }
     
+    next_id += 1
     print("Expense Added Successfully")
     expenses.append(expense_data)
+
+
+
 
 
 def view_expenses():
@@ -31,6 +49,7 @@ def view_expenses():
     for index, expense in enumerate(expenses, start=1):
         print(f"\nExpense #{index}")
         print("-" * 40)
+        print(f"Expense ID       : {expense['id']}")
         print(f"Category        : {expense['category']}")
         print(f"Description     : {expense['description']}")
         print(f"Amount          : ₹{expense['amount']}")
